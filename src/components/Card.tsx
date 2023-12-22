60594fbd5b61965758df60758011c6b1c5dcbd7f
Artur Souza <artur.souza@liferay.com>
2023-12-22 18:35:44 -0300

    Add IApiData interface and icons and check the weather condition to change the styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "../styles/card.scss";
 import classNames from "classnames";
+import {
+  FaArrowLeft,
+  FaLongArrowAltUp,
+  FaLongArrowAltDown,
+} from "react-icons/fa";
+import "../styles/card.scss";
+import { Link } from "react-router-dom";
 
 interface ICard {
   cityName: string;
@@ -13,11 +19,21 @@ enum WeatherCodition {
   SNOWY = "snowy",
 }
 
-export default function Card({ cityName }: ICard) {
-  const [weatherData, setWeatherData] = useState(null);
+interface IApiData {
+  current: {
+    condition: ICondition;
+    humidity: number;
+    temp_c: string;
+    wind_kph: number;
+  };
+  forecast: { forecastday: IForecastDayData[] };
+}
 
-  const getData = async () => {
-    await axios
+export default function Card({ cityName }: ICard) {
+  const [weatherData, setWeatherData] = useState<IApiData>();
+
+  const getData = () => {
+    axios
       .get(
         `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${cityName}`
       )
@@ -32,19 +48,32 @@ export default function Card({ cityName }: ICard) {
       const weatherCodition =
         weatherData.forecast.forecastday[0].day.condition.text.toLowerCase();
 
-      switch (weatherCodition) {
-        case WeatherCodition.SUNNY:
-          return "sunny";
-        case WeatherCodition.RAINY:
-          return "rainy";
-        case WeatherCodition.SNOWY:
-          return "snowy";
-        default:
-          return "other-condition";
-      }
+      return isWeatherCondition(weatherCodition);
     }
+    return "";
   };
 
+  const isWeatherCondition = (weather: string) => {
+    if (
+      weather.includes("rain") ||
+      weather.includes("overcast") ||
+      weather.includes("mist") ||
+      weather.includes("drizzle")
+    ) {
+      return WeatherCodition.RAINY;
+    } else if (
+      weather.includes("snow") ||
+      weather.includes("blizzard") ||
+      weather.includes("sleet") ||
+      weather.includes("freezing") ||
+      weather.includes("fog") ||
+      weather.includes("ice")
+    ) {
+      return WeatherCodition.SNOWY;
+    } else {
+      return WeatherCodition.SUNNY;
+    }
+  };
   useEffect(() => {
     getData();
   }, []);
@@ -53,131 +82,161 @@ export default function Card({ cityName }: ICard) {
     <div>
       {weatherData ? (
         <div className={classNames("card__container", checkWeatherCodition())}>
-          <header></header>
-          <div className="card__container-title">
-            <h1>{cityName}</h1>
-            <p>{weatherData.forecast.forecastday[0].day.condition.text}</p>
-          </div>
-          <div className="card__container-degree">
-            <div className="card__container-degree-current">
-              <h1>{parseInt(weatherData.current.temp_c)}</h1>
+          <div className="card__container-content">
+            <header>
+              <Link to="/">
+                <FaArrowLeft size={30} />
+              </Link>
+            </header>
+            <div className="card__container-title">
+              <h1>{cityName}</h1>
+              <p>{weatherData.forecast.forecastday[0].day.condition.text}</p>
             </div>
-            <div className="card__container-degree-others">
-              <p>°C</p>
-              <p>
-                {parseInt(weatherData.forecast.forecastday[0].day.maxtemp_c)}°
-              </p>
-              <p>
-                {parseInt(weatherData.forecast.forecastday[0].day.mintemp_c)}°
-              </p>
+            <div className="card__container-degree">
+              <div className="card__container-degree-current">
+                <h1>{parseInt(weatherData.current.temp_c)}</h1>
+              </div>
+              <span className="card__container-degree-others">
+                <p>°C</p>
+                <span className="card__container-degree-others-maxMin">
+                  <p>
+                    <FaLongArrowAltUp
+                      size={13}
+                      color={
+                        checkWeatherCodition() === "snowy" ? "black" : "white"
+                      }
+                    />
+                    {parseInt(
+                      weatherData.forecast.forecastday[0].day.maxtemp_c
+                    )}
+                    °
+                  </p>
+                  <p>
+                    <FaLongArrowAltDown
+                      size={13}
+                      color={
+                        checkWeatherCodition() === "snowy" ? "black" : "white"
+                      }
+                    />
+                    {parseInt(
+                      weatherData.forecast.forecastday[0].day.mintemp_c
+                    )}
+                    °
+                  </p>
+                </span>
+              </span>
             </div>
+            <div className="card__container-icon">
+              <img
+                alt="weather icon"
+                src={weatherData.current.condition.icon}
+              />
+            </div>
+            <div className="card__container-weather-period">
+              <ul>
+                <li>
+                  <div>
+                    <p>dawn</p>
+                    <img
+                      alt="dawn weather icon"
+                      src={
+                        weatherData.forecast.forecastday[0].hour[4].condition
+                          .icon
+                      }
+                    />
+                    <p>
+                      {parseInt(
+                        weatherData.forecast.forecastday[0].hour[4].temp_c
+                      )}
+                      °C
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <p>morning</p>
+                    <img
+                      alt="morning weather icon"
+                      src={
+                        weatherData.forecast.forecastday[0].hour[10].condition
+                          .icon
+                      }
+                    />
+                    <p>
+                      {parseInt(
+                        weatherData.forecast.forecastday[0].hour[10].temp_c
+                      )}
+                      °C
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <p>afternoon</p>
+                    <img
+                      alt="afternoon weather icon"
+                      src={
+                        weatherData.forecast.forecastday[0].hour[16].condition
+                          .icon
+                      }
+                    />
+                    <p>
+                      {parseInt(
+                        weatherData.forecast.forecastday[0].hour[16].temp_c
+                      )}
+                      °C
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <p>night</p>
+                    <img
+                      alt="night weather icon"
+                      src={
+                        weatherData.forecast.forecastday[0].hour[22].condition
+                          .icon
+                      }
+                    />
+                    <p>
+                      {parseInt(
+                        weatherData.forecast.forecastday[0].hour[22].temp_c
+                      )}
+                      °C
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <footer className="card__container-footer">
+              <ul>
+                <li>
+                  <span>
+                    <p>wind speed</p>
+                    <p>{weatherData.current.wind_kph} m/s</p>
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    <p>sunrise</p>
+                    <p>{weatherData.forecast.forecastday[0].astro.sunrise}</p>
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    <p>sunset</p>
+                    <p>{weatherData.forecast.forecastday[0].astro.sunset}</p>
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    <p>humidity</p>
+                    <p>{weatherData.current.humidity}%</p>
+                  </span>
+                </li>
+              </ul>
+            </footer>
           </div>
-          <div className="card__container-icon">
-            <img alt="weather icon" src={weatherData.current.condition.icon} />
-          </div>
-          <div className="card__container-weather-day">
-            <ul>
-              <li>
-                <div>
-                  <p>dawn</p>
-                  <img
-                    alt=""
-                    src={
-                      weatherData.forecast.forecastday[0].hour[4].condition.icon
-                    }
-                  />
-                  <p>
-                    {parseInt(
-                      weatherData.forecast.forecastday[0].hour[4].temp_c
-                    )}
-                    °C
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <p>morning</p>
-                  <img
-                    alt=""
-                    src={
-                      weatherData.forecast.forecastday[0].hour[10].condition
-                        .icon
-                    }
-                  />
-                  <p>
-                    {parseInt(
-                      weatherData.forecast.forecastday[0].hour[10].temp_c
-                    )}
-                    °C
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <p>afternoon</p>
-                  <img
-                    alt=""
-                    src={
-                      weatherData.forecast.forecastday[0].hour[16].condition
-                        .icon
-                    }
-                  />
-                  <p>
-                    {parseInt(
-                      weatherData.forecast.forecastday[0].hour[16].temp_c
-                    )}
-                    °C
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <p>night</p>
-                  <img
-                    alt=""
-                    src={
-                      weatherData.forecast.forecastday[0].hour[22].condition
-                        .icon
-                    }
-                  />
-                  <p>
-                    {parseInt(
-                      weatherData.forecast.forecastday[0].hour[22].temp_c
-                    )}
-                    °C
-                  </p>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <footer className="card__container-footer">
-            <ul>
-              <li>
-                <span>
-                  <p>wind speed</p>
-                  <p>{weatherData.current.wind_kph} m/s</p>
-                </span>
-              </li>
-              <li>
-                <span>
-                  <p>sunrise</p>
-                  <p>{weatherData.forecast.forecastday[0].astro.sunrise}</p>
-                </span>
-              </li>
-              <li>
-                <span>
-                  <p>sunset</p>
-                  <p>{weatherData.forecast.forecastday[0].astro.sunset}</p>
-                </span>
-              </li>
-              <li>
-                <span>
-                  <p>humidity</p>
-                  <p>{weatherData.current.humidity}%</p>
-                </span>
-              </li>
-            </ul>
-          </footer>
         </div>
       ) : (
         <h1>Loading data...</h1>
